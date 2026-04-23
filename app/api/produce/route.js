65736453928bfe9e1615ea9155b7dab32e8a3ef1
mimidupont <michel.dupont@seznam.cz>
2026-04-23 18:02:31 +0200

    Update route.js

--- app/api/produce/route.js
+++ app/api/produce/route.js
@@ -6,8 +6,8 @@ import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICE_ROLE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
 export async function POST() {
